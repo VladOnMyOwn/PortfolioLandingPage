add4daf3bf6191ae315a29994e1461c9b2866be3
VladOnMyOwn <76261338+VladOnMyOwn@users.noreply.github.com>
2021-08-02 19:34:49 +0300

    Add files via upload

---
+++ Project/js/ProjectScripts.js
@@ -0,0 +1,10 @@
+// Скрипт для слайдшоу
+var m = ['images/bg_im1.jpg', 'images/bg_im2.jpg', 'images/bg_im3.jpg', 'images/bg_im4.jpg'];
+var i = 0;
+function next_img()
+{
+	i++;
+	if (i >= m.length) i = 0;
+	document.bg_img_name.src = m[i];
+}
+setInterval('next_img();', 3000);
